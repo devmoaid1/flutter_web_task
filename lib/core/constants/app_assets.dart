f098053037f05ef8218dbbd7d42f65747aa9261b
devmoaid1 <moaidmohamed1312@gmail.com>
2024-10-03 17:13:27 +0300

    added assets

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,9 @@
+class AppAssets {
+  static const String calendar = 'assets/calendar.svg';
+  static const String filter = 'assets/filter.svg';
+  static const String logo = 'assets/logo.svg';
+  static const String notifications = 'assets/notifications.svg';
+  static const String options = 'assets/options.svg';
+  static const String profile = 'assets/profile.png';
+  static const String settings = 'assets/settings.svg';
+}
